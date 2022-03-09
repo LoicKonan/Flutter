f5444a618e68c720ec800be12e0030ef8799c29e
Loic <konanloic10@gmail.com>
2022-03-09 01:01:41 -0600

    README.md

--- Flutter Projects/A01/hello_world/lib/main.dart
+++ Flutter Projects/A01/hello_world/lib/main.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() 
-{
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget 
-{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) 
-  {
-    return MaterialApp
-    (
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData
-      (
+      theme: ThemeData(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
@@ -34,8 +29,7 @@ class MyApp extends StatelessWidget
   }
 }
 
-class MyHomePage extends StatefulWidget 
-{
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
@@ -53,14 +47,11 @@ class MyHomePage extends StatefulWidget
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> 
-{
+class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() 
-  {
-    setState(() 
-    {
+  void _incrementCounter() {
+    setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
@@ -71,28 +62,23 @@ class _MyHomePageState extends State<MyHomePage>
   }
 
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold
-    (
-      appBar: AppBar
-      (
+    return Scaffold(
+      appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our app bar title.
+        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center
-      (
+      body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column
-        (
+        child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -108,22 +94,18 @@ class _MyHomePageState extends State<MyHomePage>
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>
-          [
-            const Text
-            (
+          children: <Widget>[
+            const Text(
               'You have pushed the button this many times:',
             ),
-            Text
-            (
+            Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton
-      (
+      floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
